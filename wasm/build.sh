2de82ce929feb66eb366467c0c872353d6ff185a
Armani Ferrante <armaniferrante@gmail.com>
2019-04-15 20:36:36 -0700

    Cleanup examples

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# Remove this script once the following issue is addressed:
+# https://github.com/rustwasm/wasm-pack/issues/313.
+
 set -e
 
-# Check if jq is installed
+# Check if jq is installed.
 if ! [ -x "$(command -v jq)" ]; then
     echo "jq is not installed" >& 2
     exit 1
 fi
 
-# Clean previous packages
+# Clean previous packages.
 if [ -d "pkg" ]; then
     rm -rf pkg
 fi
@@ -17,14 +20,13 @@ if [ -d "pkg-node" ]; then
     rm -rf pkg-node
 fi
 
-# Build for both targets
-wasm-pack build -t nodejs -d pkg-node
-wasm-pack build -t browser -d pkg
+PKG_NAME="pure-evm"
 
-# Get the package name
-PKG_NAME=$(jq -r .name pkg/package.json | sed 's/\-/_/g')
+# Build for both targets.
+wasm-pack build -t nodejs -d pkg-node --out-name $PKG_NAME
+wasm-pack build -t browser -d pkg --out-name $PKG_NAME
 
-# Merge nodejs & browser packages
+# Merge nodejs & browser packages.
 cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_main.js"
 sed "s/require[\(]'\.\/${PKG_NAME}/require\('\.\/${PKG_NAME}_main/" "pkg-node/${PKG_NAME}_bg.js" > "pkg/${PKG_NAME}_bg.js"
 jq ".files += [\"${PKG_NAME}_bg.js\"]" pkg/package.json \
--- wasm/examples/node/index.js
+++ wasm/examples/node/index.js
@@ -1,7 +1,12 @@
 // Referencing the built package. Replace with require('pure-evm').
-let pure_evm = require('./pkg');
-let output = pure_evm.exec(bytecode(), data());
-console.log("output = ", output);
+const pure_evm = require('../../pkg');
+const assert = require('assert');
+
+const output = pure_evm.exec(bytecode(), data());
+
+const expected = Buffer.from([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7]);
+
+assert.equal(expected.toString('hex'), Buffer.from(output).toString('hex'));
 
 /**
  * Deployed bytecode *not* the initcode for the following contract.
@@ -15,7 +20,7 @@ console.log("output = ", output);
  */
 function bytecode() {
   let bytecode = '608060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806308316796146044575b600080fd5b348015604f57600080fd5b50607960048036036020811015606457600080fd5b8101908080359060200190929190505050608f565b6040518082815260200191505060405180910390f35b600060038201905091905056fea165627a7a723058200e912ad05dca5252a91d1ce28dda0451a49092178c344ac1a40ccf9c9d5d46150029';
-  return new Buffer(bytecode, 'hex');
+  return Buffer.from(bytecode, 'hex');
 }
 
 /**
@@ -23,5 +28,5 @@ function bytecode() {
  */
 function data() {
   let data = '083167960000000000000000000000000000000000000000000000000000000000000004';
-  return new Buffer(data, 'hex');
+  return Buffer.from(data, 'hex');
 }
